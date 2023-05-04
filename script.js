556ecf447d8e6e0578bb869ed065c1c15009e381
Jordan Cox <jordancox747@outlook.com>
2023-05-04 11:13:59 -0600

    clear slate to begin work on stack algorithm

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 //Global constants
-//const operationsPattern = /^[+\-*/^]+$/g;
-//const parenthesesPattern = /^[()]+$/g;
-//const numbersPattern = /^\d+$/g;
 const errorMessage = "Syntax error!";
 
 // On input
@@ -28,148 +25,28 @@ equals.addEventListener("click", () => enterInput(document.querySelector("#input
 function enterInput(input) {
     //console.log(input + "enterInput");
     let inputArray = input.match(/\d+\.\d+|\d+|[+\-/*^()]/g);
-    const output = [];
+    const infixExp = [];
 
     //console.log(inputArray);
     for (let i = 0; i < inputArray.length; i++) {
 
         if (!isNaN(Number(inputArray[i]))) {
-            output.push(Number(inputArray[i]));
+            infixExp.push(Number(inputArray[i]));
         } else {
-            output.push(inputArray[i]);
+            infixExp.push(inputArray[i]);
         }
     }
     //console.log(output + "test"); //Testing purposes
-    calculate(output);
+    changeToPostfix(infixExp);
 }
 
-/*function calculate(operations) {
-    let subOperations = [];
-    // base case
-    if (operations.length == 1) {
-        console.log(operations[0]);
-        return operations[0];
-    }
+function changeToPostfix(expression) {
+    console.log(expression); //Testing
 
-    else if (operations.includes("(")) {
-        subOperations = operations.slice(operations.indexOf("(") + 1, operations.indexOf(")"));
-        calculate(subOperations);
-    }
-
-    else if (operations.includes("*")) {
-        subOperations = [operations[operations.indexOf("*") - 1] * operations[operations.indexOf("*") + 1]];
-        calculate(subOperations);
-    }
-}*/
-
-function calculate(input) {
-    if (input.includes("-")) {
-        input = changeSign(input);
-    }
-
-    console.log(input); //Testing
-
-    if (input.includes("(")) {
-        input = parentheses(input);
-    }
+    evaluateExp(expression);
 }
 
-function changeSign(input) {
-    let newArray = input;
-
-    for (let i = 0; i < newArray.length; i++) { // test value: 1-3*-4-(8*9-4*-(4*6))
-        let value = newArray[i]; // minus sign
-        let valueBefore = newArray[i - 1]; // num before minus sign
-        let numToChange = newArray[i + 1]; // num or parentheses after minus sign
-        let start = newArray.slice(0, i);
-        let end = newArray.slice(i + 2, newArray.length);
-
-        if (value === "-" && Number(valueBefore) && numToChange !== "(") { //case num - num
-            if (isNaN(numToChange * - 1)) {
-                throw errorMessage;
-            }
-            newArray = start.concat("+").concat(numToChange * - 1).concat(end);
-        } else if (value === "-" && isNaN(valueBefore) && numToChange !== "(") { // case num operator - num and starts with -num
-            if (isNaN(numToChange * - 1)) {
-                throw errorMessage;
-            }
-            newArray = start.concat(numToChange * - 1).concat(end);
-        } else if (value === "-" && Number(valueBefore) && numToChange === "(") { // case num - ()
-            end = newArray.slice(i + 1, newArray.length);
-            newArray = start.concat("+").concat(-1).concat("*").concat(end);
-        } else if (value === "-" && isNaN(valueBefore) && numToChange === "(") { // case num operator - ()
-            end = newArray.slice(i + 1, newArray.length);
-            newArray = start.concat(-1).concat("*").concat(end);
-        }
-    }
-
-    return newArray;
-}
-
-function parentheses(input) { // test value: ((4+5)+(4*(4-(5^(3-4)))))
-    let newArray = input;
-    let subArray = [];
-    let counter = 0;
-    let start = 0;
-    let end = 0;
-
-    if (!newArray.includes("(")) {
-        console.log("base case reached!"); // Begin searching performing operations
-        console.log(newArray);
-        return 1;
-    } else {
-        for (let i = 0; i < newArray.length; i++) {
-            if (newArray[i] === "(" && counter === 0) {
-                start = i + 1;
-                counter++;
-            } else if (newArray[i] === "(" && counter > 0) {
-                counter++;
-                //console.log(counter);
-            } else if (newArray[i] === ")" && counter > 1) {
-                counter--;
-                //console.log(counter);
-            } else if (newArray[i] === ")" && counter === 1) {
-                counter--;
-                end = i;
-                subArray = newArray.slice(start, end);
-                //console.log(subArray);
-                let x = parentheses(subArray);
-                 // store subArray and continue until no more parentheses. Feed back through...
-                //console.log(start + " " + end);
-                console.log(x);
-                console.log(`${start} and ${end}`);
-            }
-        }
-    }
-
-    return newArray;
-}
-
-function sqrt() {
-
-}
-
-function cbrt() {
-
-}
-
-function root() {
-
-}
-
-function exponent() {
-
-}
-
-function multiply() {
-
-}
-
-function divide() {
-
-}
-
-function add() {
+function evaluateExp(expression) {
 
 }
 
